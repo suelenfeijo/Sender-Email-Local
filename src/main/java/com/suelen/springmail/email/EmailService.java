c03a603b9c137cfbf182e579b9365b8631a1eb7e
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-21 18:47:54 -0300

    add método send em EmailService

--- src/main/java/com/suelen/springmail/email/EmailService.java
+++ src/main/java/com/suelen/springmail/email/EmailService.java
@@ -1,14 +1,16 @@
 package com.suelen.springmail.email;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Getter
 @Setter
@@ -24,6 +26,19 @@ public class EmailService implements EmailSender{
 	@Override
 	public void send(String to, String email) {
 		
+		
+      try {
+    	  MimeMessage mimeMessage = mailSender.createMimeMessage();
+          MimeMessageHelper helper =
+                  new MimeMessageHelper(mimeMessage, "utf-8");
+          helper.setText(email, true);
+          helper.setTo(to);
+          helper.setSubject("Confirm your email");
+          helper.setFrom("hello@amigoscode.com");
+	 } catch (MessagingException e) {
+         LOGGER.error("falha ao enviar o email", e);
+         throw new IllegalStateException("failed to send email");
+     }
+      
 	}
-
 }
