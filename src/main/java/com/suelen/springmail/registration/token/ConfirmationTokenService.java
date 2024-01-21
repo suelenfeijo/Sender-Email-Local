4a542c60ad061095eebeedd6ebe8fdc7dd93d397
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-21 18:06:21 -0300

    add saveConfirmationToken

--- src/main/java/com/suelen/springmail/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/suelen/springmail/registration/token/ConfirmationTokenService.java
@@ -1,5 +1,18 @@
 package com.suelen.springmail.registration.token;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
 
+	
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
 }
