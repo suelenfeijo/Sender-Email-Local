c9dabf08fba9eb77145ccfc7f37a93ada4225e78
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-21 17:48:32 -0300

    add regex confirmação de email

---
+++ src/main/java/com/suelen/springmail/registration/EmailValidator.java
@@ -0,0 +1,15 @@
+package com.suelen.springmail.registration;
+
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailValidator implements Predicate<String>{
+
+	@Override
+	public boolean test(String s) {
+		return true;
+	}
+}
