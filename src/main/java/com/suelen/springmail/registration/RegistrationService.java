86c2882667cd00637123c83e3d0b7b15272693af
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-21 17:55:00 -0300

    adicionando lógica de registro de usuário

--- src/main/java/com/suelen/springmail/registration/RegistrationService.java
+++ src/main/java/com/suelen/springmail/registration/RegistrationService.java
@@ -2,10 +2,13 @@ package com.suelen.springmail.registration;
 
 import org.springframework.stereotype.Service;
 
+import com.suelen.springmail.appuser.AppUser;
+import com.suelen.springmail.appuser.AppUserRole;
+import com.suelen.springmail.appuser.AppUserService;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Service
 @Getter
@@ -13,9 +16,32 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RegistrationService {
 
+	private final EmailValidator emailValidator;
+	private final AppUserService appUserService;
 
+	public String register(RegistrationRequest request) {
+		boolean isValidEmail = emailValidator.test(request.getEmail());
+		if (!isValidEmail) {
+			throw new IllegalArgumentException("o email não é válido");
+		}
 
-    public String register(RegistrationRequest request) {
-       return "works";
-    }
+		String token = appUserService.signUpUser
+			  (new AppUser(request.getFirstName(),
+		        request.getLastName(),
+				request.getEmail(),
+				request.getPassword(),
+				AppUserRole.USER
+
+		)
+	);
+		return token;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
