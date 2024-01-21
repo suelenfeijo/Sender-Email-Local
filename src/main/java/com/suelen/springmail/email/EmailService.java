1c61c8957961432a64540db378a48761fd2ae845
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-21 18:40:32 -0300

    add class JavaMailSender em EmailService

--- src/main/java/com/suelen/springmail/email/EmailService.java
+++ src/main/java/com/suelen/springmail/email/EmailService.java
@@ -1,6 +1,19 @@
 package com.suelen.springmail.email;
 
+import org.springframework.mail.javamail.JavaMailSender;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class EmailService implements EmailSender{
+
+	
+    private final JavaMailSender mailSender;
 
 	@Override
 	public void send(String to, String email) {
